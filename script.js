8873a35b2c17483abb1032d78e81bf8b2c6d3d56
sarthu1503 <sarthakgaikwad020@gmail.com>
2024-10-01 08:46:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    voiceSelect.innerHTML = "";   
+        voices.forEach((voice, i) => {
+        let option = document.createElement("option");
+        option.value = i;
+        option.textContent = voice.name;
+        voiceSelect.appendChild(option);
+    });
+};
+
+voiceSelect.addEventListener("change", () => {
+    speech.voice = voices[voiceSelect.value];
+});
+
+document.querySelector("button").addEventListener("click", () => {
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+});
